38e01deec05ac3de19d2e8d9060386221e1126a9
João Pedro Uggioni <uggionidaminj@gmail.com>
2025-12-26 22:19:03 -0300

    implementa a função de renderização de tarefas na lista

--- js/script.js
+++ js/script.js
@@ -10,4 +10,27 @@ let tarefas = [
     'Ler um livro'
 ]
 
+function renderizarTarefas() {
+    for(tarefa of tarefas){
+
+        //criar um item na lista
+        let itemLista = document.createElement('li');
+
+        //adicionar classes do bootstrap no item da lista
+        itemLista.setAttribute('class', 'list-group-item list-group-item-action');
+
+        //criar um texto
+        let itemTexto = document.createTextNode(tarefa);
+
+        // Adicionar o texto no item da lista
+        itemLista.appendChild(itemTexto);
+
+        // Adicionar o item da lista na lista
+        lista.appendChild(itemLista);
+
+    }
+}
+
+renderizarTarefas();
+
 //<li class="list-group-item list-group-item-action">Jogar</li>
