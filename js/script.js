e3a013d1f3fc654420d4f9548ef023ccbf19ca18
João Pedro Uggioni <uggionidaminj@gmail.com>
2025-12-26 22:06:48 -0300

    criação e início do javaScript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Referenciar o input, o button e a lista
+
+let input = document.querySelector('input[name=tarefa]');   
+let button = document.querySelector('#botao');
+let lista = document.querySelector('#lista');
+
+let tarefas = [
+    'Estudar JavaScript',
+    'Fazer exercícios',
+    'Ler um livro'
+]
+
+//<li class="list-group-item list-group-item-action">Jogar</li>
