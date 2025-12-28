85ef4019b548030399ec3dcbb37f04882157934d
João Pedro Uggioni <uggionidaminj@gmail.com>
2025-12-28 12:44:23 -0300

    Inserindo dados na aplicação: Adicionado função ao clicar no botão de adicionar tarefa, com mensagem de erro caso esteja com input vazio.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 // Referenciar o input, o button e a lista
 
 let input = document.querySelector('input[name=tarefa]');   
-let button = document.querySelector('#botao');
+let btn = document.querySelector('#botao');
 let lista = document.querySelector('#lista');
+
+//varável global
+let card = document.querySelector('.card');
+
 
 let tarefas = [
     'Estudar JavaScript',
@@ -11,6 +15,11 @@ let tarefas = [
 ]
 
 function renderizarTarefas() {
+
+    //limpar a lista antes de renderizar
+    lista.innerHTML ='';
+
+    //percorrer o array de tarefas
     for(tarefa of tarefas){
 
         //criar um item na lista
@@ -33,4 +42,41 @@ function renderizarTarefas() {
 
 renderizarTarefas();
 
-//<li class="list-group-item list-group-item-action">Jogar</li>
+//escutando o clique no botão
+btn.onclick = function (){
+    //recuperar o valor digitado no input
+    let novaTarefa = input.value;
+
+    if(novaTarefa !== ''){
+        //adicionar a nova tarefa na lista (array)
+    tarefas.push(novaTarefa);
+
+    //executa a função para renderizar a lista
+    renderizarTarefas();
+
+    //limpar o input 
+    input.value ='';
+
+    removerSpans();
+
+    }else{
+        removerSpans();
+
+        let span = document.createElement('span');
+        span.setAttribute('class', 'alert alert-warning');
+
+        let msg = document.createTextNode('Você precisa informar a tarefa!');
+        span.appendChild(msg);
+
+        card.appendChild(span); 
+
+    }
+}
+
+function removerSpans(){
+    let spans = document.querySelectorAll('span');
+
+    for(let i=0; i<spans.length; i++){
+        card.removeChild(spans[i]);
+    }
+}
